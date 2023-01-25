2ac11f43c759eb6fd1be89b4a9009adbf6118f50
Enaleth <la.corte.renaud@gmail.com>
2023-01-25 19:05:33 +0100

    First Commit

---
+++ DotNetCore/Model/Department.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNetCore.Model
+{
+    public class Department
+    {
+        public Department()
+        {
+            Positions = new HashSet<Position>();
+        }
+        [Key]
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public virtual ICollection<Position> Positions { get; set; }
+    }
+}
