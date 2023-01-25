5824da0a26f84cc4079039cdb8a62236ec4c1ce6
Enaleth <la.corte.renaud@gmail.com>
2023-01-25 21:01:31 +0100

    Added API Delete method

--- DotNetCore/Controllers/SalariesController.cs
+++ DotNetCore/Controllers/SalariesController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DotNetCore.Model;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace DotNetCore.Controllers
 {
@@ -7,5 +9,32 @@ namespace DotNetCore.Controllers
     [ApiController]
     public class SalariesController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var db = new ApiDbContext();
+
+            var list = db.Salaries.ToList();
+            return Ok(list);
+        }
+
+        [HttpDelete("{Id}")]
+        public IActionResult Delete(int Id)
+        {
+            var db = new ApiDbContext();
+
+            Salary salary = db.Salaries.Find(Id);
+
+            if (salary == null)
+            {
+                return NotFound();
+            }
+
+            db.Salaries.Remove(salary);
+
+            db.SaveChanges();
+
+            return NoContent();
+        }
     }
 }
