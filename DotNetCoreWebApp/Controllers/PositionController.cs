6e9d343650d91df2d842629b538c405829e0e7a3
Enaleth <la.corte.renaud@gmail.com>
2023-01-29 13:52:58 +0100

    Added position feature

---
+++ DotNetCoreWebApp/Controllers/PositionController.cs
@@ -0,0 +1,103 @@
+﻿using DotNetCoreWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCoreWebApp.Controllers
+{
+    public class PositionController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<Position> list = new List<Position>();
+            HttpClient client = new HttpClient();
+            HttpResponseMessage responseMessage = await client.GetAsync("http://localhost:51336/api/positions");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jstring = await responseMessage.Content.ReadAsStringAsync();
+                list = JsonConvert.DeserializeObject<List<Position>>(jstring);
+                return View(list);
+            }
+
+            return View(list);
+        }
+        public IActionResult Add()
+        {
+            Position position = new Position();
+            return View(position);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Position position)
+        {
+            if (ModelState.IsValid)
+            {
+                HttpClient client = new HttpClient();
+                var jsonDepartment = JsonConvert.SerializeObject(position);
+                StringContent content = new StringContent(jsonDepartment, Encoding.UTF8, "application/json");
+                HttpResponseMessage message = await client.PostAsync("http://localhost:51336/api/positions", content);
+                if (message.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                ModelState.AddModelError("Error", "There is an error");
+                return View(position);
+            }
+
+            return View(position);
+        }
+
+        public async Task<IActionResult> Update(int Id)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage message = await client.GetAsync("http://localhost:51336/api/positions/" + Id);
+
+            if (message.IsSuccessStatusCode)
+            {
+                var jstring = await message.Content.ReadAsStringAsync();
+                Position position = JsonConvert.DeserializeObject<Position>(jstring);
+                return View(position);
+            }
+
+            return RedirectToAction("Update");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Position position)
+        {
+            if (ModelState.IsValid)
+            {
+                HttpClient client = new HttpClient();
+                var jsonposition = JsonConvert.SerializeObject(position);
+                StringContent content = new StringContent(jsonposition, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage message = await client.PutAsync("http://localhost:51336/api/positions", content);
+                if (message.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                return View(position);
+            }
+
+            return View(position);
+        }
+        public async Task<IActionResult> Delete(int Id)
+        {
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage message = await client.DeleteAsync("http://localhost:51336/api/positions/" + Id);
+            if (message.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DotNetCoreWebApp/Models/Position.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetCoreWebApp.Models
+{
+    public class Position
+    {
+        [Key]
+        public int PositionId { get; set; }
+        [Required(ErrorMessage = "Please fill position name")]
+        public string Name { get; set; }
+    }
+}
